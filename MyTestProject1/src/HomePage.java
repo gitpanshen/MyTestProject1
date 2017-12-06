1ad38bbedbfb2e482aaa9717d166ae4fcf8d5921
pans <pans@CND5304L2Z.bljc.com>
2017-12-06 15:38:17 -0500

    test

---
+++ MyTestProject1/src/HomePage.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.WebElement;
+
+import org.openqa.selenium.support.FindBy;
+
+import org.openqa.selenium.support.PageFactory;
+
+
+
+public class HomePage {
+
+	WebDriver driver = myTestCase1.browser;
+	
+	public HomePage() {
+		
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	@FindBy(id = "site-header")
+	WebElement siteHeader;
+	
+	@FindBy(xpath = "/html/body/div[1]/div[2]/div[1]/div/div[2]/div/div[2]/a[1]")
+	WebElement freeTrailButton;
+	
+	public void verifySiteHeader(){
+		assertTrue((siteHeader.isDisplayed()));   
+	}
+	
+	public void clickFreeTrail() {
+		freeTrailButton.click();
+	}
+	
+}
--- MyTestProject1/src/myTestCase1.java
+++ MyTestProject1/src/myTestCase1.java
@@ -1,36 +1,39 @@
-import static org.junit.Assert.*;
-
 import org.junit.After;
 
 import org.junit.Before;
 
 import org.junit.Test;
 
-import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.lang.*;
+
+
 public class myTestCase1 {
+	
+	public static WebDriver browser;
 
 	@Before
 	public void beforeEachTest() {
+		        		
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		
+		browser = new ChromeDriver();
+		
+		browser.get("https://saucelabs.com");   
 		   
 	 }
 	
-	@Test
+	/*@Test
 	public void testWithFireFox() {
 		
 		WebDriver driver;   
 
 		//Firefox's geckodriver requires you to specify its location.    
 		
-		System.setProperty("webdriver.gecko.driver","C:\\Users\\pansh\\Downloads\\geckodriver-v0.19.1-win32\\geckodriver.exe");   
+		System.setProperty("webdriver.gecko.driver","E:\\Selenium\\geckodriver-v0.19.1-win32\\geckodriver.exe");   
 
 		driver = new FirefoxDriver();     
 		
@@ -41,28 +44,27 @@ public class myTestCase1 {
 		assertTrue((header.isDisplayed()));   
 
 		driver.close();
-	}
+	}*/
 	
 	@Test
-	public void  testWithChrome() {
+	public void  verifySiteHeaderTest() {
 		
-		WebDriver browser;
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\pansh\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		
-		browser = new ChromeDriver();
-		
-		browser.get("http://saucelabs.com");   
-
-		WebElement header = browser.findElement(By.id("site-header"));   
-
-		assertTrue((header.isDisplayed()));   
-
-		browser.close();
+		HomePage homePage = new HomePage();
+		homePage.verifySiteHeader();
+				
+	}
+	
+	@Test 
+	public void  gotoFreeTrailTest() throws InterruptedException {
+		HomePage homePage = new HomePage();
+		homePage.clickFreeTrail();
+		Thread.sleep(2000);
 	}
 		
 	 @After
 	 public void afterEachTest() {
+		 
+		 browser.quit();
 		 
 	 }
 	 
